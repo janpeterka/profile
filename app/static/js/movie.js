483005f0a8107baa946d269dd570a1ebc7e92da1
Jan Peterka <janmpeterka@gmail.com>
2020-01-10 01:53:06 +0100

    Add most of movie

--- app/static/js/movie.js
+++ app/static/js/movie.js
@@ -1,87 +1,134 @@
 var videoContainer = document.getElementById("video");
 var videoPlayer = document.getElementById("videoPlayer");
+videoPlayer.addEventListener('ended', handler_video_end, false);
+var videoSource = document.getElementById("videoSource");
+
 var menu = document.getElementById("menu");
+var optionsLabel = document.getElementById("optionsLabel");
 var menuOptions = document.getElementById("options");
 var optionList = document.getElementById("optionList");
 
+var currentVideoCode = "0";
+
 var json_source = `{
 	"menus":
-	[{
+	{
+		"0": {
+			"label": "Game",
+			"options": [
+				{
+					"label": "Start",
+					"code": "1"
+				}
+			]
+
+		},
+
 		"1": {
-			"code_from": "1",
-				"options": [{
-					"first": {
-						"label": "Go left",
-						"code": "2a"
-					},
-					"second": {
-						"label": "Go right",
-						"code": "2b"
-					}
-				}]
+			"label": "Where do you want to go?",
+			"options": [
+				{
+					"label": "Go right",
+					"code": "2a"
+				},
+				{
+					"label": "Go left",
+					"code": "2b"
+				}
+			]
 		},
 		
 		"2": {
-			"code_from": "2",
-				"options": [{
-					"first": {
-						"label": "Turn around",
-						"code": "3a"
-					},
-					"second": {
-						"label": "Go right",
-						"code": "4"
-					}
-				}]
+			"label": "What do you do?",
+			"options": [
+				{
+					"label": "Turn around and go right",
+					"code": "3a"
+				},
+				{
+					"label": "Go right",
+					"code": "3b"
+				}
+			]
 		},
 
-		"4": {
-			"code_from": "4",
-				"options": [{
+		"3": {
+				"options": {
 					"first": {
 						"label": "Go",
-						"code": "5a"
+						"code": "4a"
 					},
 					"second": {
 						"label": "Run!",
-						"code": "5b"
+						"code": "4b"
 					}
-				}]
+				}
 		}
 
-
-	}]
+	}
 
 }
 `
-
 json = JSON.parse(json_source);
 
-function start_game(){
-	playVideo()
-	get_menu_options();
+function prepare_game(){
+	set_menu_options(currentVideoCode);
+}
+
+function select_option(code){
+	console.log("selected: " + code);
+	setVideo(code);
+	playVideo();
 }
 
 function get_menu_options() {
-	var currentId = menu.dataset.currentId;
-	set_menu_options(currentId);
+	currentVideoId = currentVideoCode.match(/\d+/g)[0];
+	set_menu_options(currentVideoId);
 }
 
 function set_menu_options(id){
-	
-	options = json.menus[id].options;
+	// console.log(id);
+	situation = json["menus"][id]
+	options = situation["options"];
+
+	// Prepare situation label
+	$(optionsLabel).empty();
+	optionsLabel.innerHTML = "<h3>"+situation["label"]+"<h3>"
+
+
+	// Prepare option list
 	$(optionList).empty();
 
 	for (var i = options.length - 1; i >= 0; i--) {
-		$("<li><a id='"+options[i].code+"'>"+options[i].label+"</li>").appendTo(optionList);
-
+		$("<li><a onclick='select_option(\""+options[i]["code"]+"\")' id='"+options[i]["code"]+"'>"+options[i]["label"]+"</li>").appendTo(optionList);
 	}
+
+	menu.classList.remove("hide")
+}
+
+function setVideo(code) {
+	videoSource.setAttribute('src', "static/videos/"+code+".webm");
+    currentVideoCode = code
 }
 
 function playVideo() {
+
     // video.preload = "auto";
     videoPlayer.load();
+    videoContainer.classList.remove("blurred");
+    // videoContainer.classList.add("unblurred");
+    menu.classList.add("hide");
     videoPlayer.play();
-    // return video;
 }
 
+function handler_video_end(e) {
+	get_menu_options();
+    videoContainer.classList.add("blurred");
+    // videoContainer.classList.remove("unblurred");
+}
+
+
+
+
+
+window.onload = prepare_game()
