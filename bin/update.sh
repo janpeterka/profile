57d963968a28b7b430631d6a999554924e2db9f8
Jan Peterka <jan.peterka@hey.com>
2022-10-31 14:24:16 +0100

    Add update script

---
+++ bin/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/profile/profile
+
+# ensure to get origin's data and discard any local change
+git fetch
+git reset origin/production --hard
+git pull
+
+# update pipenv
+pipenv sync
+
+# upgrade database
+pipenv run flask db upgrade
+
